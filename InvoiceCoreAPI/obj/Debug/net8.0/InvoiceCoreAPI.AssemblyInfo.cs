3dd9004cb40b2dd7e3ef01bdb830eeee5a9e1c9d
valavan05 <valavansangar@gmail.com>
2026-06-21 20:32:56 +0530

    Database Script Query and User CRUD Operation

--- InvoiceCoreAPI/obj/Debug/net8.0/InvoiceCoreAPI.AssemblyInfo.cs
+++ InvoiceCoreAPI/obj/Debug/net8.0/InvoiceCoreAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InvoiceCoreAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67128aeaf7489799a78e53d5ab0ce321a5700491")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+004f4e63534db726aee36fbc024ab534ec158845")]
 [assembly: System.Reflection.AssemblyProductAttribute("InvoiceCoreAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InvoiceCoreAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
